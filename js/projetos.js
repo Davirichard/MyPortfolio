69a7282c5f392d07b5ac306d731330e34e13e065
David <programadordev18@gmail.com>
2023-11-29 00:37:13 -0300

    Portfolio

--- js/projetos.js
+++ js/projetos.js
@@ -1,12 +1,8 @@
+
+
 document.addEventListener('DOMContentLoaded', function () {
     const filterLinks = document.querySelectorAll('.filter-link');
     const projects = document.querySelectorAll('.projetos');
-    const maxVisibleProjects = 6; // Defina o número máximo de projetos visíveis inicialmente
-
-    // Ocultar projetos além do número máximo definido
-    projects.forEach((project, index) => {
-        project.style.display = index < maxVisibleProjects ? 'block' : 'none';
-    });
 
     filterLinks.forEach(link => {
         link.addEventListener('click', function (event) {
@@ -25,16 +21,10 @@ document.addEventListener('DOMContentLoaded', function () {
             this.style.color = '#F74B0A';
 
             // Otimização: percorrer os projetos uma vez e definir a exibição com base nas tags
-            projects.forEach((project, index) => {
+            projects.forEach(project => {
                 const projectTags = project.getAttribute('data-tags').split(' ');
 
-                if (selectedTag === 'all') {
-                    // Se a tag selecionada for "all", mostrar todos os projetos
-                    project.style.display = 'block';
-                } else {
-                    // Caso contrário, mostrar apenas os projetos com a tag correspondente
-                    project.style.display = projectTags.includes(selectedTag) ? 'block' : 'none';
-                }
+                project.style.display = (projectTags.includes(selectedTag) || selectedTag === 'all') ? 'block' : 'none';
             });
         });
     });
